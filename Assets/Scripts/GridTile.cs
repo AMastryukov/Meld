6bcd16facc797cb9a0d5d2c8a0b15ad64ef5216b
Alexander <Alexandermastryukov@gmail.com>
2017-08-04 17:47:13 -0400

    Implemented tile grid generation, added basic tile sprites

---
+++ Assets/Scripts/GridTile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour {
+
+  private int tileID;
+  private bool isWall;
+  private bool isMeldable;
+
+  SpriteRenderer spriteRenderer;
+
+  void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
+  public int TileID
+  {
+    get
+    {
+      return tileID;
+    }
+    set
+    {
+      tileID = value;
+    }
+  }
+  public bool IsWall
+  {
+    get
+    {
+      return isWall;
+    }
+    set
+    {
+      isWall = value;
+    }
+  }
+  public bool IsMeldable
+  {
+    get
+    {
+      return isMeldable;
+    }
+    set
+    {
+      isMeldable = value;
+    }
+  }
+}
