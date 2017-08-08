5ed02ce064fad370f081be1747fc5ad98b5e6028
Alexander <Alexandermastryukov@gmail.com>
2017-08-08 19:42:53 -0400

    Added camera deadzone

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
   public GameObject player;
+  public int deadzone;
 
   private Vector3 offset;
   
 	void Start () {
+
     offset = transform.position - player.transform.position;
 	}
 	
-	void LateUpdate () {
-    transform.position = player.transform.position + offset;
+	void LateUpdate ()
+  {
+    // get mouse position
+    Vector3 mousePosition = Input.mousePosition;
+    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+    transform.position = player.transform.position + offset + 
+      new Vector3(
+        -(transform.position.x - mousePosition.x) / deadzone, 
+        -(transform.position.y - mousePosition.y) / deadzone,
+        0.0f);
 	}
 }
