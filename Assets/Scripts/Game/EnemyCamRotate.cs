18198f5d8498f90991f8a9f0a4e5a4a4c7c63feb
Alexander <Alexandermastryukov@gmail.com>
2017-08-10 16:54:42 -0400

    Added rotating enemy camera functionality

---
+++ Assets/Scripts/Game/EnemyCamRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCamRotate : MonoBehaviour {
+  
+  public int rotationSpeed;
+  public int rotationMax;
+
+  float startingRotation;
+  float totalRotation;
+
+  private void Start()
+  {
+    startingRotation = transform.rotation.z;
+    totalRotation = rotationMax / 2;
+  }
+  
+  void Update () {
+    transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
+    totalRotation += rotationSpeed * Time.deltaTime;
+
+    if (Mathf.Abs(totalRotation) >= rotationMax)
+    {
+      rotationSpeed = -rotationSpeed;
+      totalRotation = 0;
+    }
+
+    Debug.Log(totalRotation);
+  }
+}
